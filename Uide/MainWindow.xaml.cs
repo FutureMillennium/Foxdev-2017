519b0bd25ca471801bcd2d719bae01576a9e7a6e
Zdeněk Gromnica <futuremillennium@gmail.com>
2017-01-10 22:58:16 +0100

    + dummy IDE window 	+ drop file into window 	+ read file + .gitignore

---
+++ Uide/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uide
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Window_DragEnter(object sender, DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+			{
+				e.Effects = DragDropEffects.Link;
+				e.Handled = true;
+			}
+		}
+
+		private void Window_Drop(object sender, DragEventArgs e)
+		{
+			if (e.Data.GetDataPresent(DataFormats.FileDrop))
+			{
+				string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+				if (files.Length > 0)
+				{
+					string filename = files[0];
+
+					try
+					{
+						byte[] file = File.ReadAllBytes(filename);
+						// TODO
+					}
+					catch (Exception ex)
+					{
+						// TODO show non-intrusively inside app
+						MessageBox.Show(this, "Something went wrong!" + Environment.NewLine + Environment.NewLine + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+					}
+
+				}
+			}
+		}
+	}
+}

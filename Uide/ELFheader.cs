1611fd5715a608a136978ef2e7d5f2bc91962477
Zdeněk Gromnica <futuremillennium@gmail.com>
2017-01-18 00:03:21 +0100

    + ELF magic number validation ! Unfinished: + ELF header class + ELF header hypothetical custom language code example

---
+++ Uide/ELFheader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uide
+{
+	class ELFheader
+	{
+		public enum ClassBitNumber : byte { Bit32, Bit64 };
+		public enum Endianness : byte { LittleEndian, BigEndian };
+
+		public ClassBitNumber bitNumber; // e_ident[EI_CLASS]
+		public Endianness endianness; // e_ident[EI_DATA]
+		public byte version; // e_ident[EI_VERSION];
+		public byte targetOS; // e_ident[EI_OSABI]
+							  // e_ident[EI_ABIVERSION]
+							  // e_ident[EI_PAD]
+	}
+}
--- Uide/MainWindow.xaml.cs
+++ Uide/MainWindow.xaml.cs
@@ -43,17 +43,32 @@ namespace Uide
 				string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
 				if (files.Length > 0)
 				{
-					string filename = files[0];
+					string filename = files[0]; // TODO more than 1 file
+					byte[] file;
 
 					try
 					{
-						byte[] file = File.ReadAllBytes(filename);
-						// TODO
+						file = File.ReadAllBytes(filename);
 					}
 					catch (Exception ex)
 					{
 						// TODO show non-intrusively inside app
 						MessageBox.Show(this, "Something went wrong!" + Environment.NewLine + Environment.NewLine + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+						file = null;
+					}
+
+					if (file != null
+						&& file.Length > 3
+						&& file[0] == 0x7F
+						&& file[1] == 'E'
+						&& file[2] == 'L'
+						&& file[3] == 'F')
+					{
+						this.Title = "Ayyy";
+					}
+					else
+					{
+						this.Title = "Nay";
 					}
 
 				}

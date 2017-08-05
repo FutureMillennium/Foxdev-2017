85b60c77bc6a5cdb4135c440b30e9faa3ff63ff1
Zdeněk Gromnica <futuremillennium@gmail.com>
2017-08-05 23:35:05 +0200

    - removed double buffering function (should be unnecessary: “Standard Windows Forms controls are double buffered by default.”)

--- Uide/MainForm.cs
+++ Uide/MainForm.cs
@@ -21,30 +21,12 @@ namespace Uide
 
 		Font font = new Font(FontFamily.GenericMonospace, 13);
 
-		void SetDoubleBuffered(System.Windows.Forms.Control c)
-		{
-			//Taxes: Remote Desktop Connection and painting
-			//http://blogs.msdn.com/oldnewthing/archive/2006/01/03/508694.aspx
-			if (System.Windows.Forms.SystemInformation.TerminalServerSession)
-				return;
 
-			System.Reflection.PropertyInfo aProp =
-				  typeof(System.Windows.Forms.Control).GetProperty(
-						"DoubleBuffered",
-						System.Reflection.BindingFlags.NonPublic |
-						System.Reflection.BindingFlags.Instance);
-
-			aProp.SetValue(c, true, null);
-		}
-
-		
 
 
 		public MainForm(string[] args)
 		{
 			InitializeComponent();
-
-			SetDoubleBuffered(this.mainBox);
 
 			MainForm_Resize(null, null);
 
